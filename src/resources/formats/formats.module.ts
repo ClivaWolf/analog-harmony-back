23b1a6eacc72be700dee1e09fe21e75e6003e0b0
Cliva Wolf <cliva.wolf@mail.ru>
2024-11-29 07:54:17 +0300

    fix tipeORM

--- src/resources/formats/formats.module.ts
+++ src/resources/formats/formats.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FormatsService } from './formats.service';
 import { FormatsController } from './formats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from '../items/entities/item.entity';
+import { Format } from './entities/format.entity';
 
 @Module({
   controllers: [FormatsController],
   providers: [FormatsService],
+  imports: [
+    TypeOrmModule.forFeature([Format]),
+  ],
 })
 export class FormatsModule {}
--- src/resources/orders/orders.module.ts
+++ src/resources/orders/orders.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order_item.entity';
 
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
+  imports: [
+    TypeOrmModule.forFeature([Order,OrderItem]),
+  ],
 })
 export class OrdersModule {}
